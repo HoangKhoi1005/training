e7aed4ed15c191c51a6a9424775f5f022d4969d2
KhanhNT <khanhnt748@gmail.com>
2025-05-28 19:50:45 +0700

    demo git commit

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import 'screen/home.dart';
@@ -12,6 +14,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    print('Hello there');
     return MaterialApp(
       title: 'Training',
       theme: ThemeData(
